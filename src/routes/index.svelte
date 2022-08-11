c79db3dcac007960c1ef8ef7beec8101a45e2b00
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-11 16:03:12 -0400

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Frontend Mentor | REST Countries API</title>
+</svelte:head>
+
+<main class="m-auto">
+  <h1 class="text-4xl">Hello World</h1>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/app.html", "./src/**/*.{svelte,ts}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blue: {
+        100: "hsl(209, 23%, 22%)",
+        200: "hsl(207, 26%, 17%)",
+        300: "hsl(200, 15%, 8%)",
+      },
+      gray: {
+        100: "hsl(0, 0%, 98%)",
+        200: "hsl(0, 0%, 52%)",
+      },
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontWeight: {
+      light: "300",
+      normal: "600",
+      bold: "800",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Nunito\\ Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
