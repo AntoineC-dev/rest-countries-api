bee47d445fde900f29a97661737886a2a4901396
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-12 03:38:32 -0400

    Fix filtering helpers

---
+++ src/helpers/filtering.helpers.ts
@@ -0,0 +1,15 @@
+import type { CountrySimple } from "../types";
+
+export const filterCountries = (
+  countries: CountrySimple[],
+  { name, region }: { name: string; region: string }
+): CountrySimple[] => {
+  if (!!name && !!region)
+    return countries.filter(
+      (country) =>
+        country.region.toLowerCase() === region.toLowerCase() && country.name.toLowerCase().includes(name.toLowerCase())
+    );
+  if (!!name) return countries.filter((country) => country.name.toLowerCase().includes(name.toLowerCase()));
+  if (!!region) return countries.filter((country) => country.region.toLowerCase() === region.toLowerCase());
+  return countries;
+};
