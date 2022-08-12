f4dd857b81d0278b9336073dc6143444adc57eb4
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-12 15:59:00 -0400

    Add components to

---
+++ src/lib/components/CountryCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { CountrySimple } from "$lib/types";
+
+  export let country: CountrySimple;
+  const href = `/countries/${country.name.toLowerCase()}`;
+</script>
+
+<a {href} class="flex flex-col max-w-sm w-full bg-white dark:bg-blue-100 shadow-custom rounded-md overflow-hidden">
+  <img src={country.flag} alt="" />
+  <div class="flex flex-col px-6 pt-8 pb-12 gap-6">
+    <span class="text-lg font-bold leading-none">{country.name}</span>
+    <div class="flex flex-col gap-3 text-sm leading-none">
+      <span>Population: <span class="font-light">{country.population}</span></span>
+      <span>Region: <span class="font-light">{country.region}</span></span>
+      <span>Capital: <span class="font-light">{country.capital}</span></span>
+    </div>
+  </div>
+</a>
---
+++ src/lib/components/SearchInput.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { debounce } from "$lib/helpers";
+
+  let value: string = "";
+
+  const dispatch = createEventDispatcher<{ search: { name: string } }>();
+  const onChange = debounce(() => dispatch("search", { name: value.trim() }), 500); // Runs only if user stops typing for 500ms
+  $: value && onChange();
+
+  const onClear = () => {
+    value = "";
+    dispatch("search", { name: value });
+  };
+</script>
+
+<div
+  class="flex items-center px-4 py-2 gap-2 bg-white dark:bg-blue-100 shadow-custom w-full sm:max-w-md xl:max-w-lg rounded-md"
+>
+  <svg xmlns="http://www.w3.org/2000/svg" class="w-5 aspect-auto m-2" viewBox="0 0 20 20" fill="currentColor">
+    <path
+      fill-rule="evenodd"
+      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+      clip-rule="evenodd"
+    />
+  </svg>
+
+  <input
+    type="text"
+    aria-label="Search for a country by name"
+    bind:value
+    placeholder="Search for a country..."
+    class="bg-transparent text-sm outline-none p-2 w-full"
+  />
+  {#if !!value}
+    <button type="button" aria-label="Clear search bar" on:click={onClear} class="p-2">
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 aspect-auto" viewBox="0 0 20 20" fill="currentColor">
+        <path
+          fill-rule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </button>
+  {/if}
+</div>
