9398d9ced7388411001d3608bec606b0c4abc1f9
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-12 21:54:01 -0400

    Add simple cache system

--- src/routes/countries/[name].ts
+++ src/routes/countries/[name].ts
@@ -2,7 +2,17 @@ import type { RequestHandler } from "@sveltejs/kit";
 import type { CountryComplete } from "$lib/types";
 import { getCountryByName } from "$lib/api";
 
+let cacheKeys: string[] = [];
+let cacheData: Record<string, CountryComplete> = {};
+
 export const GET: RequestHandler<{ name: string }, { country: CountryComplete }> = async ({ params }) => {
-  const country = await getCountryByName(params.name);
+  let country: CountryComplete;
+  if (cacheKeys.includes(params.name)) {
+    country = cacheData[params.name];
+  } else {
+    country = await getCountryByName(params.name);
+    cacheKeys = [...cacheKeys, params.name];
+    cacheData[params.name] = country;
+  }
   return { body: { country } };
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,15 @@ import type { RequestHandler } from "@sveltejs/kit";
 import type { CountrySimple } from "$lib/types";
 import { getAllCountries } from "$lib/api";
 
+let cacheData: CountrySimple[] = [];
+
 export const GET: RequestHandler<{}, { countries: CountrySimple[] }> = async () => {
-  const countries = await getAllCountries();
+  let countries: CountrySimple[] = [];
+  if (cacheData.length === 0) {
+    countries = await getAllCountries();
+    cacheData = [...countries];
+  } else {
+    countries = cacheData;
+  }
   return { body: { countries } };
 };
