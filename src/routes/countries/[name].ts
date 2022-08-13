8ee0ec08dd88d0e4f0193dd38f473dd8f7f0d75b
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-12 22:41:37 -0400

    Remove dev cache features

--- src/routes/countries/[name].ts
+++ src/routes/countries/[name].ts
@@ -2,17 +2,7 @@ import type { RequestHandler } from "@sveltejs/kit";
 import type { CountryComplete } from "$lib/types";
 import { getCountryByName } from "$lib/api";
 
-let cacheKeys: string[] = [];
-let cacheData: Record<string, CountryComplete> = {};
-
 export const GET: RequestHandler<{ name: string }, { country: CountryComplete }> = async ({ params }) => {
-  let country: CountryComplete;
-  if (cacheKeys.includes(params.name)) {
-    country = cacheData[params.name];
-  } else {
-    country = await getCountryByName(params.name);
-    cacheKeys = [...cacheKeys, params.name];
-    cacheData[params.name] = country;
-  }
+  const country = await getCountryByName(params.name);
   return { body: { country } };
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,15 +2,7 @@ import type { RequestHandler } from "@sveltejs/kit";
 import type { CountrySimple } from "$lib/types";
 import { getAllCountries } from "$lib/api";
 
-let cacheData: CountrySimple[] = [];
-
 export const GET: RequestHandler<{}, { countries: CountrySimple[] }> = async () => {
-  let countries: CountrySimple[] = [];
-  if (cacheData.length === 0) {
-    countries = await getAllCountries();
-    cacheData = [...countries];
-  } else {
-    countries = cacheData;
-  }
+  const countries = await getAllCountries();
   return { body: { countries } };
 };
