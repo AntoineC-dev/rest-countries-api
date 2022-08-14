713e8e9a42ff4f44619252ce423ae7342302a68d
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-14 19:00:55 -0400

    Remove debug logs

--- src/routes/countries/[code].ts
+++ src/routes/countries/[code].ts
@@ -3,7 +3,6 @@ import type { CountryComplete } from "$lib/types";
 import { getCountryByCode } from "$lib/api";
 
 export const GET: RequestHandler<{ code: string }, { country: CountryComplete }> = async ({ params }) => {
-  console.log(params);
   const country = await getCountryByCode(params.code);
   return { body: { country } };
 };
